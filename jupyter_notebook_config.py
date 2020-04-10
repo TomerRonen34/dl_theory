a43fe075525bbc9ab593363953622774a1762d10
Tomer Ronen <tomer.ronen34@gmail.com>
2020-04-10 22:03:05 +0300

    better extensions from jupyter_notebook_config

--- jupyter_notebook_config.py
+++ jupyter_notebook_config.py
@@ -5,11 +5,15 @@ import re
 
 
 def convert_notebook_to_html(work_dir, fname):
-    check_call(['jupyter', 'nbconvert', '--to', 'html', '--no-prompt', fname], cwd=work_dir)
+    output_fname = fname + '.html'  # extension: .ipynb.html
+    check_call(['jupyter', 'nbconvert', '--to', 'html', '--no-prompt',
+                '--output', output_fname, fname], cwd=work_dir)
 
 
 def convert_notebook_to_python(work_dir, fname):
-    check_call(['jupyter', 'nbconvert', '--to', 'python', '--no-prompt', fname], cwd=work_dir)
+    output_fname = fname + '.py'  # extension: .ipynb.py
+    check_call(['jupyter', 'nbconvert', '--to', 'python', '--no-prompt',
+                '--output', output_fname, fname], cwd=work_dir)
 
 
 def convert_notebook(os_path):
