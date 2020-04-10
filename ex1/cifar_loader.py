f9382a06bdd4e4633488ddb8ffc67ec640389456
Tomer Ronen <tomer.ronen34@gmail.com>
2020-04-11 00:32:37 +0300

    finished cifar loader

--- ex1/cifar_loader.py
+++ ex1/cifar_loader.py
@@ -5,7 +5,12 @@ from typing import *
 from nptyping import *
 
 
-def load_cifar_dataset(dataset_dir):
+def load_cifar_dataset(dataset_dir: str
+                       ) -> Tuple[NDArray[(Any, 32, 32, 3), np.uint8],
+                                  NDArray[Any, int],
+                                  NDArray[(Any, 32, 32, 3), np.uint8],
+                                  NDArray[Any, int],
+                                  NDArray[Any, str]]:
     train_paths, test_path, meta_path = _generate_paths(dataset_dir)
     train_images, train_labels = _load_multiple_batches(train_paths)
     test_images, test_labels = _load_batch(test_path)
@@ -25,7 +30,7 @@ def _generate_paths(dataset_dir: str
 
 def _load_multiple_batches(batch_paths: Iterable[str]
                            ) -> Tuple[NDArray[(Any, 32, 32, 3), np.uint8],
-                                      NDArray[int]]:
+                                      NDArray[Any, int]]:
     loaded_batches = [_load_batch(path) for path in batch_paths]
     images, labels = zip(*loaded_batches)
     images = np.concatenate(images, axis=0)
@@ -35,11 +40,11 @@ def _load_multiple_batches(batch_paths: Iterable[str]
 
 def _load_batch(batch_path: str
                 ) -> Tuple[NDArray[(Any, 32, 32, 3), np.uint8],
-                           NDArray[int]]:
+                           NDArray[Any, int]]:
     batch_dict = _load_cifar_file(batch_path)
     labels = np.asarray(batch_dict[b"labels"])
     image_vectors = np.asarray(batch_dict[b"data"])
-    images = _parse_image_vectors(image_vectors)
+    images = _unpack_image_vectors(image_vectors)
     return images, labels
 
 
@@ -50,27 +55,34 @@ def _load_cifar_file(path: str
     return cifar_dict
 
 
-def _parse_image_vectors(image_vectors: NDArray[(Any, 32 * 32 * 3), np.uint8]
-                         ) -> NDArray[(Any, 32, 32, 3), np.uint8]:
-    # TODO: numpy-ize
-    images = []
-    for _image_vector in image_vectors:
-        _image = np.reshape(_image_vector, (3, 32, 32)).transpose((1, 2, 0))
-        images.append(_image)
-    images = np.array(images)
+def _unpack_image_vectors(image_vectors: NDArray[(Any, 32 * 32 * 3), np.uint8]
+                          ) -> NDArray[(Any, 32, 32, 3), np.uint8]:
+    images = np.reshape(image_vectors, (-1, 3, 32, 32)
+                        ).transpose((0, 2, 3, 1))
     return images
 
 
 def _load_class_names(meta_path: str
-                      ) -> List[str]:
+                      ) -> NDArray[Any, str]:
     cifar_meta_dict = _load_cifar_file(meta_path)
     bytes_class_names = cifar_meta_dict[b"label_names"]
-    class_names = [cls.decode() for cls in bytes_class_names]
+    class_names = np.array([cls.decode() for cls in bytes_class_names])
     return class_names
 
 
 def _example():
-    train_images, train_labels = load_cifar_dataset("cifar-10-batches-py")
+    train_images, train_labels, test_images, test_labels, class_names = (
+        load_cifar_dataset("cifar-10-batches-py"))
+
+    import matplotlib.pyplot as plt
+    def show_image(image, title):
+        plt.figure()
+        plt.imshow(image)
+        plt.title(title)
+        plt.show()
+
+    show_image(train_images[0], class_names[train_labels[0]])
+    show_image(test_images[0], class_names[test_labels[0]])
     pass
 
 
