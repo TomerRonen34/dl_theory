8bcd89bec1c52dd5ad42fd3c6b873e5db58e1e03
Tomer Ronen <tomer.ronen34@gmail.com>
2020-04-10 21:43:33 +0300

    Added jupyter_notebook_config for notebook version control. This config makes jupyter run a post_save hook which saves a notebook in .html and .py formats.
    Overall it's a nice solution, only problem is that creating .html files takes some time, so if you save your notebook a lot it slows your execution as the kernel has to wait.
    
    -  If you change the code, all change (.ipynb, .html, .py) and it's easy to see the changes in the .py file.
    -  If you change the output but not the code (for example print a random number), only .ipynb and .html change
    -  If you change only the execution count, only the .ipynb file changes

---
+++ jupyter_notebook_config.py
@@ -0,0 +1,37 @@
+# Reference: https://svds.com/jupyter-notebook-best-practices-for-data-science/
+import os.path as osp
+from subprocess import check_call
+import re
+
+
+def convert_notebook_to_html(work_dir, fname):
+    check_call(['jupyter', 'nbconvert', '--to', 'html', '--no-prompt', fname], cwd=work_dir)
+
+
+def convert_notebook_to_python(work_dir, fname):
+    check_call(['jupyter', 'nbconvert', '--to', 'python', '--no-prompt', fname], cwd=work_dir)
+
+
+def convert_notebook(os_path):
+    work_dir, fname = osp.split(os_path)
+    convert_notebook_to_html(work_dir, fname)
+    convert_notebook_to_python(work_dir, fname)
+
+
+def post_save(model, os_path, contents_manager):
+    """post-save hook for converting notebooks to .py scripts and .html files"""
+    if model['type'] == 'notebook':
+        convert_notebook(os_path)
+
+
+def check_post_save():
+    model = {"type": "notebook"}
+    os_path = osp.abspath("my_notebook.ipynb")
+    contents_manager = None
+    post_save(model, os_path, contents_manager)
+
+
+if not __name__ == '__main__':
+    c.FileContentsManager.post_save_hook = post_save
+else:
+    check_post_save()
