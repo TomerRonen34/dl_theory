a08fe2e6b9708cf31eab3af8588ff5201197832e
Tomer Ronen <tomer.ronen34@gmail.com>
2020-04-11 23:59:50 +0300

    calculating and plotting the confusion matrix

--- ex1/ex1.ipynb.py
+++ ex1/ex1.ipynb.py
@@ -15,43 +15,67 @@ import pickle
 import os.path as osp
 
 import torch
-
 from sklearn.svm import SVC
-from sklearn.metrics import classification_report, confusion_matrix, ConfusionMatrixDisplay, accuracy_score
 
 from cifar_loader import load_cifar_dataset
-from utils import shuffle_multiple_arrays, plot_model_results
+from utils import shuffle_multiple_arrays, fit_and_save, eval_and_plot
 
 
 seed = 34
+save_dir = "models"
 
 
 # ### Prepare data
 
 orig_train_images, orig_train_labels, test_images, test_labels, class_names = load_cifar_dataset("cifar-10-batches-py")
 train_images, train_labels = shuffle_multiple_arrays(orig_train_images, orig_train_labels, seed=seed)
+
 X_train = train_images.reshape(len(train_images), -1)
 X_test = test_images.reshape(len(test_images), -1)
+y_train = train_labels.copy()
+y_test = test_labels.copy()
 
 
-# n = 100
-# X_train, train_labels, X_test, test_labels = X_train[:n], train_labels[:n], X_test[:n], test_labels[:n]
+n = 20
+X_train, y_train, X_test, y_test = X_train[:n], y_train[:n], X_test[:n], y_test[:n]
 
 
 # ## Linear SVM
 
 model = SVC(kernel="linear", random_state=seed)
-model.fit(X_train, train_labels)
-y_pred = model.predict(X_test)
-y_true = test_labels
-plot_model_results(y_true, y_pred, class_names, model_name="Linear SVM")
+model_name = "linear_svm"
+model_display_name = "Linear SVM"
+
+fit_and_save(model, X_train, y_train,
+             save_dir, model_name)
+
+eval_and_plot(model, X_test, y_test,
+              class_names, save_dir, model_name, model_display_name)
 
 
 # ## SVM with RBF Kernel
 
+model = SVC(kernel="linear", random_state=seed)
+model_name = "linear_svm"
+model_display_name = "Linear SVM"
+
+fit_and_save(model, X_train, y_train,
+             save_dir, model_name)
+
+eval_and_plot(model, X_test, y_test,
+              class_names, save_dir, model_name, model_display_name)
+
+
 model = SVC(kernel="rbf", random_state=seed)
-model.fit(X_train, train_labels)
-y_pred = model.predict(X_test)
-y_true = test_labels
-plot_model_results(y_true, y_pred, class_names, model_name="SVM with RBF Kernel")
+model_name = "rbf_svm"
+model_display_name = "SVM with RBF Kernel"
+
+fit_and_save(model, X_train, y_train,
+             save_dir, model_name)
+
+eval_and_plot(model, X_test, y_test,
+              class_names, save_dir, model_name, model_display_name)
+
+
+
 
--- ex1/utils.py
+++ ex1/utils.py
@@ -1,17 +1,37 @@
 from typing import *
 from nptyping import *
+
+import os
+import os.path as osp
 import numpy as np
-from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, accuracy_score
+import pickle
 import matplotlib.pyplot as plt
 
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, accuracy_score
 
-def shuffle_multiple_arrays(*arrays: NDArray, seed: int = None
-                            ) -> Tuple[NDArray, ...]:
-    length = len(arrays[0])
-    random_state = np.random.RandomState(seed)
-    perm = random_state.permutation(length)
-    shuf_arrays = tuple([x[perm] for x in arrays])
-    return shuf_arrays
+
+def fit_and_save(model, X_train, y_train,
+                 save_dir, model_name):
+    model.fit(X_train, y_train)
+    save_model(model, save_dir, model_name)
+    return model
+
+
+def eval_and_plot(model, X_test, y_test,
+                  class_names, save_dir, model_name, model_display_name):
+    y_pred = model.predict(X_test)
+    disp = plot_model_results(y_true=y_test, y_pred=y_pred,
+                              class_names=class_names, model_display_name=model_display_name)
+    save_confusion_matrix_display(disp, save_dir, model_name)
+
+
+def plot_model_results(y_true, y_pred, class_names=None, model_display_name=None, figsize=(10, 10)):
+    model_display_name = model_display_name if model_display_name is not None else ''
+    accuracy = accuracy_score(y_true, y_pred)
+    title = f"{model_display_name}\nAccuracy = {accuracy:.2f}"
+    disp = plot_confusion_matrix(y_true, y_pred, class_names, figsize=figsize, title=title)
+    plt.show()
+    return disp
 
 
 def plot_confusion_matrix(y_true, y_pred, class_names=None, figsize=(10, 10), normalize="true", title=None):
@@ -25,14 +45,48 @@ def plot_confusion_matrix(y_true, y_pred, class_names=None, figsize=(10, 10), no
 
     cm = confusion_matrix(y_true, y_pred, labels=labels, normalize=normalize)
     disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=class_names)
+
     fig, ax = plt.subplots(figsize=figsize)
     disp.plot(ax=ax, cmap=plt.cm.Blues, xticks_rotation="vertical")
-    disp.ax_.set_title(title)
-    plt.show()
+    prettify_confusion_matrix_display(disp, title)
+
+    return disp
 
 
-def plot_model_results(y_true, y_pred, class_names=None, model_name=None, figsize=(10, 10)):
-    model_name = model_name if model_name is not None else ''
-    accuracy = accuracy_score(y_true, y_pred)
-    title = f"{model_name}\nAccuracy = {accuracy:.2f}"
-    plot_confusion_matrix(y_true, y_pred, class_names, figsize=figsize, title=title)
+def prettify_confusion_matrix_display(disp: ConfusionMatrixDisplay,
+                                      title: str):
+    disp.ax_.set_title(title, {"fontsize": "x-large"})
+    disp.ax_.tick_params(labelsize="large")
+    disp.ax_.set_xlabel(disp.ax_.get_xlabel(), {"fontsize": "x-large"})
+    disp.ax_.set_ylabel(disp.ax_.get_ylabel(), {"fontsize": "x-large"})
+
+
+def save_confusion_matrix_display(disp: ConfusionMatrixDisplay,
+                                  save_dir: str,
+                                  model_name: str):
+    os.makedirs(save_dir, exist_ok=True)
+    save_path = osp.join(save_dir, model_name + ".png")
+    disp.figure_.savefig(save_path, bbox_inches="tight")
+
+
+def save_model(model, save_dir, model_name):
+    os.makedirs(save_dir, exist_ok=True)
+    save_path = osp.join(save_dir, model_name + ".pkl")
+    with open(save_path, 'wb') as f:
+        pickle.dump(model, f)
+
+
+def load_model(save_dir, model_name):
+    save_path = osp.join(save_dir, model_name + ".pkl")
+    with open(save_path, 'rb') as f:
+        model = pickle.load(f)
+    return model
+
+
+def shuffle_multiple_arrays(*arrays: NDArray, seed: int = None
+                            ) -> Tuple[NDArray, ...]:
+    length = len(arrays[0])
+    random_state = np.random.RandomState(seed)
+    perm = random_state.permutation(length)
+    shuf_arrays = tuple([x[perm] for x in arrays])
+    return shuf_arrays
