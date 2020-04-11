c7c0e4bc27131c13e53f806d85dde4af12b6d0ba
Tomer Ronen <tomer.ronen34@gmail.com>
2020-04-11 03:21:34 +0300

    organized the notebook

--- ex1/ex1.ipynb.py
+++ ex1/ex1.ipynb.py
@@ -26,23 +26,32 @@ from utils import shuffle_multiple_arrays, plot_model_results
 seed = 34
 
 
+# ### Prepare data
+
 orig_train_images, orig_train_labels, test_images, test_labels, class_names = load_cifar_dataset("cifar-10-batches-py")
 train_images, train_labels = shuffle_multiple_arrays(orig_train_images, orig_train_labels, seed=seed)
-
-
 X_train = train_images.reshape(len(train_images), -1)
 X_test = test_images.reshape(len(test_images), -1)
 
 
+# n = 100
+# X_train, train_labels, X_test, test_labels = X_train[:n], train_labels[:n], X_test[:n], test_labels[:n]
+
+
+# ## Linear SVM
+
 model = SVC(kernel="linear", random_state=seed)
-model.fit(X_train[:1000], train_labels[:1000])
-
-
-# y_pred = model.predict(X_test)
-# y_true = test_labels
-y_pred = model.predict(X_test[::10])
-y_true = test_labels[::10]
-
-
+model.fit(X_train, train_labels)
+y_pred = model.predict(X_test)
+y_true = test_labels
 plot_model_results(y_true, y_pred, class_names, model_name="Linear SVM")
+
+
+# ## SVM with RBF Kernel
+
+model = SVC(kernel="rbf", random_state=seed)
+model.fit(X_train, train_labels)
+y_pred = model.predict(X_test)
+y_true = test_labels
+plot_model_results(y_true, y_pred, class_names, model_name="SVM with RBF Kernel")
 
